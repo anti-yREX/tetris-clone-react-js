59fe02a1c9b175fb4ca69031c310a01c53a90443
Rituraj Ingale <ritu.raj.ingale@gmail.com>
2021-09-19 14:42:57 +0530

    initiate current block

--- src/components/PlayArea/index.jsx
+++ src/components/PlayArea/index.jsx
@@ -6,27 +6,113 @@ import {
 
 const MAX_COLS = 10;
 const MAX_ROWS = 20;
+
 class PlayArea extends React.Component {
     constructor(props) {
         super(props);
+        const currentBlock = this.getInitialBlock('s');
         this.state = {
-            pixels: this.getInitialPixels(),
+            pixels: this.getInitialPixels(currentBlock),
         };
     }
 
-    getInitialPixels = () => {
+    getInitialBlock = (blockString) => {
+        switch (blockString) {
+            case 's':
+                return {
+                    color: 'red',
+                    pixels: [
+                        {
+                            xCoord: 3,
+                            yCoord: 0,
+                        },
+                        {
+                            xCoord: 4,
+                            yCoord: 0,
+                        },
+                        {
+                            xCoord: 4,
+                            yCoord: 1,
+                        },
+                        {
+                            xCoord: 5,
+                            yCoord: 1,
+                        },
+                    ],
+                };
+            case 'z':
+                return {
+                    color: 'red',
+                    pixels: [
+                        {
+                            xCoord: 5,
+                            yCoord: 0,
+                        },
+                        {
+                            xCoord: 6,
+                            yCoord: 0,
+                        },
+                        {
+                            xCoord: 4,
+                            yCoord: 1,
+                        },
+                        {
+                            xCoord: 5,
+                            yCoord: 1,
+                        },
+                    ],
+                };
+            case 'l':
+                return {
+                    color: 'red',
+                    pixels: [],
+                };
+            case 'j':
+                return {
+                    color: 'red',
+                    pixels: [],
+                };
+            case 'o':
+                return {
+                    color: 'red',
+                    pixels: [],
+                };
+            case 'i':
+                return {
+                    color: 'red',
+                    pixels: [],
+                };
+            case 't':
+                return {
+                    color: 'red',
+                    pixels: [],
+                };
+            default:
+                return {
+                    color: 'red',
+                    pixels: [],
+                };
+        }
+    }
+
+    getInitialPixels = (currentBlock) => {
+        const { color, pixels } = currentBlock;
         const rowsArray = [];
         for(let i = 0; i < MAX_ROWS ; i+= 1) {
             const colsArray = [];
-            for(let j = 1; j < MAX_COLS ; j += 1) {
-                if (j === 3) {
+            for(let j = 0; j < MAX_COLS ; j += 1) {
+                if (
+                    pixels.find(cur => cur.xCoord === j && cur.yCoord === i)
+                ) {
                     colsArray.push({
+                        color,
                         isEmpty: false,
                     });
+                } else {
+                    colsArray.push({
+                        isEmpty: true,
+                    });
                 }
-                colsArray.push({
-                    isEmpty: true,
-                });
             }
             rowsArray.push(colsArray);
         }
@@ -41,6 +127,7 @@ class PlayArea extends React.Component {
                 {pixels.map(row => (
                     row.map(pixel => (
                         <Pixel
+                            color={pixel.color}
                             isEmpty={pixel.isEmpty}
                         />
                     ))
